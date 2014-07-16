192da1d941a32245a621a2d27f9988935e7db78a
pablojorge20 <pmedina@192.168.68.1>
2014-07-16 00:09:38 -0500

    Correciones

--- src/java/celepsa/rrcc/bl/TipoDocumentoBL.java
+++ src/java/celepsa/rrcc/bl/TipoDocumentoBL.java
@@ -7,40 +7,19 @@
 package celepsa.rrcc.bl;
 
 
-import celepsa.rrcc.eh.HibernateUtil;
+import celepsa.rrcc.da.TipoDocumentoDA;
 import celepsa.rrcc.eh.TmTipoDocumento;
 import java.util.List;
-import org.hibernate.Query;
-import org.hibernate.Session;
+
 
 /**
  *
  * @author pmedina
  */
 public class TipoDocumentoBL {
-    Session session= null;
-        public TipoDocumentoBL() 
+      public List<TmTipoDocumento> ListarTipoDocumento()  throws Exception
     {
-            this.session =HibernateUtil.getSessionFactory().getCurrentSession();
-        //TipoDocumentoDA objSistemaDA = new TipoDocumentoDA();
-        //return objSistemaDA.listarTipoDocumento();
-    }
-        
-public List<TmTipoDocumento> ListarTipoDocumento() throws Exception
-{
-    List<TmTipoDocumento> TDocumento = null;
-    try
-        {
-                org.hibernate.Transaction  tx =session.beginTransaction();
-                Query q =session.createQuery("From TmTipoDocumento as tipoDocumento");
-                
-                TDocumento=(List<TmTipoDocumento>) q.list();
-                
-                
-        } catch (Exception e){
-                e.printStackTrace();
-        }
-        return TDocumento;
-}
-
+        TipoDocumentoDA objSistemaDA = new TipoDocumentoDA();
+        return objSistemaDA.ListarTipoDocumento();
+    }  
 }
